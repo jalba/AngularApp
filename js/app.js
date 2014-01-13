88fefbdb8e00f59e849cdb7fe9ae3ae1a64b94de
Javier Alba <jalba@Javiers-MacBook-Pro.local>
2014-01-13 03:02:56 -0300

    new page

---
+++ js/app.js
@@ -0,0 +1,73 @@
+var app = angular.module('AngularApp', []);
+
+
+app.factory('Facebook', function($q, $rootScope) {
+    var self = this;
+    resolve = function(errval, retval, deferred) {
+        $rootScope.$apply(function() {
+            if (errval) {
+                deferred.reject(errval);
+            } else {
+                retval.connected = true;
+                deferred.resolve(retval);
+            }
+        });
+    };
+
+    return {
+        getUser: function(FB) {
+            var deferred = $q.defer();
+            FB.getLoginStatus(function(response) {
+                if (response.authResponse) {
+                    resolve(null, response, deferred);
+                    token = response.authResponse.accessToken;
+                    getFriends(FB);
+                 }
+                else {
+                     FB.login(function(response) {
+                         if (response.authResponse) {
+                             resolve(null, response, deferred); 
+                             token = response.authResponse.accessToken;
+                             getFriends(FB);
+                         }
+                         else {
+                             resolve(response.error, null, deferred);
+                             console.log('User cancelled login or did not fully authorize.');
+                         }
+                     }, {scope: 'user_location'});
+                }
+            },true);
+            promise = deferred.promise;
+            promise.connected = false;
+            return promise;
+        }
+    }
+});
+
+app.controller('AngCtrl', function($scope, Facebook) {
+    $scope.logged = Facebook.getUser(FB);
+});
+
+var getFriends = function(FB) {
+    FB.api('/me/friends', function(response){
+        if (response && response.data){
+            var friendsListContainer=document.getElementById("friendContainer");
+            var fData=response.data;
+            for (var friendIndex=0; friendIndex<fData.length; friendIndex++) {
+                var friend = document.createElement("div");
+                friend.className = "friend";
+                var picture = document.createElement("img");
+                picture.src = "https://graph.facebook.com/"+ fData[friendIndex].id +"/picture";
+                friend.appendChild(picture);
+                var name = document.createElement("b");
+                name.innerHTML = fData[friendIndex].name;
+                friend.appendChild(name);
+                friendsListContainer.appendChild(friend);
+            }
+        } else {
+            console.log('Something goes wrong', response);
+        }
+    });         
+}
+
+
